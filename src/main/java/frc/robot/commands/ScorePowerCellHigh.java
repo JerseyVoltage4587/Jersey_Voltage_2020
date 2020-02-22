401863bd8d13c862572342f7e548772c75ad8580
Team4587 <info@jerseyvoltage.com>
2020-02-21 19:49:37 -0600

    commit

--- src/main/java/frc/robot/commands/ScorePowerCellHigh.java
+++ src/main/java/frc/robot/commands/ScorePowerCellHigh.java
@@ -7,6 +7,9 @@
 
 package frc.robot.commands;
 
+import java.util.concurrent.DelayQueue;
+
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
@@ -19,6 +22,6 @@ public class ScorePowerCellHigh extends SequentialCommandGroup {
   public ScorePowerCellHigh() {
     // Add your commands in the super() call, e.g.
     // super(new FooCommand(), new BarCommand());
-    super(new Aim());
+    super(new Aim(), new ShootPowerCell());
   }
 }
